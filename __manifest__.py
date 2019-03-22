1efff62dc3b784948c758db34e7ef12874ad20b9
Wytze Jan Riedstra <wjriedstra@gmail.com>
2019-03-22 09:30:08 +0100

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) codeNext
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Portal set mail author to current user',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'codeNext',
+    'website': 'https://www.codenext.nl',
+    'category': 'Hidden',
+    'depends': [
+        'portal'
+    ],
+    'installable': True,
+}
